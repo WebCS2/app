c056e3cfb5800481c47013d6ab130324a25c91ad
WebCS2 <boomsstudiosalt@gmail.com>
2026-05-03 10:33:00 +0300

    game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("3d");
 
 function resize() {
   canvas.width = window.innerWidth;
